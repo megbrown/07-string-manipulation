972c259655ae56fb6ac41eec15ffe4683033e7fc
Megan B <brown.m.j@outlook.com>
2017-06-25 17:16:42 -0500

    when user clicks enter or submit, set the value of the string and print output of each function

--- strings.js
+++ strings.js
@@ -4,48 +4,41 @@ let outputPal = document.getElementById("palindrome");
 let inputArea = document.getElementById("input-area");
 let submit = document.getElementById("submit");
 let letters = /^[A-Za-z]+$/;
+let testString = "";
 
+inputArea.addEventListener("keyup", function(event){
+	if (event.keyCode === 13) {
+		submit.click();
+	}
+})
+
+submit.addEventListener("click", function (testString) {
+	if(inputArea.value.match(letters)) {
+		reversal(testString);
+		alphabits(testString);
+		palindrome(testString);
+	} else {
+		alert("Letters only please");
+	}
+})
 
 function reversal(testString) {
-	submit.addEventListener("click", function() {
-		if(inputArea.value.match(letters)) {
-			testString = inputArea.value.split("").reverse().join("");
-			outputRev.innerHTML = testString;
-		} else {
-			alert("Letters only please");
-		}
-	})
+	testString = inputArea.value.split("").reverse().join("");
+	outputRev.innerHTML = testString;
 }
 
 function alphabits(testString) {
-	submit.addEventListener("click", function() {
-		if(inputArea.value.match(letters)) {
-			testString = inputArea.value.split("").reverse().join("");
-			newTestString = testString.split("").sort().join("");
-			outputAlph.innerHTML = newTestString;
-		} else {
-			alert("Letters only please");
-		}
-	})
+	testString = inputArea.value.split("").reverse().join("");
+	newTestString = testString.split("").sort().join("");
+	outputAlph.innerHTML = newTestString;
 }
 
 function palindrome(testString) {
-	submit.addEventListener("click", function() {
-		if(inputArea.value.match(letters)) {
-			testString = inputArea.value.split("");
-			let length = Math.floor(testString.length/2);
-			for (i = 0; i < length; i++) {
-				if (testString[i] !== testString[testString.length - i - 1])
-					return false;
-			outputPal.innerHTML = "It's a palindrome";
-			}
-		} else {
-			alert("Letters only please");
-		}
-	})
+	testString = inputArea.value.split("");
+	let length = Math.floor(testString.length/2);
+	for (i = 0; i < length; i++) {
+		if (testString[i] !== testString[testString.length - i - 1])
+			return false;
+	outputPal.innerHTML = "It's a palindrome";
+	}
 }
-
-var testString = "";
-reversal(testString);
-alphabits(testString);
-palindrome(testString);
