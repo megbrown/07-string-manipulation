89efdc0e7b86a93e58686b0d5e5bc4994ed7ae1d
Megan B <brown.m.j@outlook.com>
2017-06-25 16:47:21 -0500

    alphabits and palindrome logic

--- strings.js
+++ strings.js
@@ -1,25 +1,48 @@
-let outputArea = document.getElementById("output-area");
+let outputRev = document.getElementById("reversal");
+let outputAlph = document.getElementById("alphabits");
+let outputPal = document.getElementById("palindrome");
 let inputArea = document.getElementById("input-area");
 let submit = document.getElementById("submit");
 let letters = /^[A-Za-z]+$/;
 
-function reversal() {
+
+function reversal(testString) {
 	submit.addEventListener("click", function() {
 		if(inputArea.value.match(letters)) {
-			let string = inputArea.value.split("").reverse().join("");
-			outputArea.innerHTML = string;
+			testString = inputArea.value.split("").reverse().join("");
+			outputRev.innerHTML = testString;
 		} else {
 			alert("Letters only please");
 		}
 	})
 }
 
-function alphabits() {
-
+function alphabits(testString) {
+	submit.addEventListener("click", function() {
+		if(inputArea.value.match(letters)) {
+			testString = inputArea.value.split("").reverse().join("");
+			newTestString = testString.split("").sort().join("");
+			outputAlph.innerHTML = newTestString;
+		} else {
+			alert("Letters only please");
+		}
+	})
 }
 
-function palindrome() {
-
+function palindrome(testString) {
+	submit.addEventListener("click", function() {
+		if(inputArea.value.match(letters)) {
+			testString = inputArea.value.split("");
+			let length = Math.floor(testString.length/2);
+			for (i = 0; i < length; i++) {
+				if (testString[i] !== testString[testString.length - i - 1])
+					return false;
+			outputPal.innerHTML = "It's a palindrome";
+			}
+		} else {
+			alert("Letters only please");
+		}
+	})
 }
 
 var testString = "";
